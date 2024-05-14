15fc7283ebca9d5c0baae8a1ef0a7347f73c5f4e
Brightnyandiko <carlsonbright98@gmail.com>
2024-05-14 15:51:24 +0300

    Product details fragment

--- app/src/main/java/com/example/ecommerce_app/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/example/ecommerce_app/fragments/categories/BaseCategoryFragment.kt
@@ -1,7 +1,52 @@
 package com.example.ecommerce_app.fragments.categories
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ecommerce_app.R
+import com.example.ecommerce_app.adapters.BestProductAdapter
+import com.example.ecommerce_app.databinding.FragmentBaseCategoryBinding
 
 open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
+    private lateinit var binding: FragmentBaseCategoryBinding
+    private lateinit var offerAdapter: BestProductAdapter
+    private lateinit var bestProductsAdapter: BestProductAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBaseCategoryBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupOfferRv()
+        setupBestProductsRv()
+    }
+
+    private fun setupBestProductsRv() {
+        bestProductsAdapter = BestProductAdapter()
+        binding.rvBestProducts.apply {
+            layoutManager = GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+            adapter = bestProductsAdapter
+        }
+    }
+
+    private fun setupOfferRv() {
+        offerAdapter = BestProductAdapter()
+        binding.rvBestProducts.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = offerAdapter
+        }
+
+    }
 }
---
+++ app/src/main/java/com/example/ecommerce_app/fragments/shopping/ProductDetailsFragment.kt
@@ -0,0 +1,4 @@
+package com.example.ecommerce_app.fragments.shopping
+
+class ProductDetailsFragment {
+}
