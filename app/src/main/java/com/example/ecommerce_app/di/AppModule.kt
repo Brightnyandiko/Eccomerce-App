6e04605fdfb38b56ef945f0aee6cb30858cbcba9
Brightnyandiko <carlsonbright98@gmail.com>
2024-05-27 09:18:09 +0300

    Added user profile settings functionality

--- app/src/main/java/com/example/ecommerce_app/di/AppModule.kt
+++ app/src/main/java/com/example/ecommerce_app/di/AppModule.kt
@@ -8,6 +8,7 @@ import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
+import com.google.firebase.storage.FirebaseStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -37,4 +38,8 @@ object AppModule {
         firebaseAuth: FirebaseAuth,
         firestore: FirebaseFirestore
     ) = FirebaseCommon(firestore, firebaseAuth)
+
+    @Provides
+    @Singleton
+    fun provideStorage() = FirebaseStorage.getInstance().reference
 }
